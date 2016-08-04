4a03e8db9c5bab773aa05162d2265de2fe6302b3
rockan007 <rockan007@gmail.com>
2016-08-04 09:39:14 +0800

    jar文件

--- app/src/main/java/com/work/jsy/jiaobao2/adapters/FirstRecyclerViewAdapter.java
+++ app/src/main/java/com/work/jsy/jiaobao2/adapters/FirstRecyclerViewAdapter.java
@@ -3,6 +3,7 @@ package com.work.jsy.jiaobao2.adapters;
 import android.content.Context;
 import android.support.v4.util.ArrayMap;
 
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
