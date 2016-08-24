fd12cb494afff4aef5886070c22808d11a97dd25
rockan007 <rockan007@gmail.com>
2016-08-24 14:07:50 +0800

    登陆界面

---
+++ app/src/main/java/com/work/jsy/jiaobao2/fragments/LoginDialogFragment.java
@@ -0,0 +1,61 @@
+package com.work.jsy.jiaobao2.fragments;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.work.jsy.jiaobao2.R;
+
+/**
+ * Created by admin on 2016/8/24.
+ */
+public class LoginDialogFragment extends DialogFragment implements View.OnClickListener {
+    public static LoginDialogFragment newInstance;
+
+    public static LoginDialogFragment getNewInstance() {
+        if (newInstance == null) {
+            newInstance = new LoginDialogFragment();
+        }
+        return newInstance;
+    }
+
+    private View mView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_login_dialog, container, false);
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews();
+    }
+
+    private void initViews() {
+        mView.findViewById(R.id.register_weiXin).setOnClickListener(this);
+        mView.findViewById(R.id.register_qq).setOnClickListener(this);
+        mView.findViewById(R.id.login_self).setOnClickListener(this);
+        mView.findViewById(R.id.register_self).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.register_weiXin:
+                break;
+            case R.id.register_qq:
+                break;
+            case R.id.login_self:
+                break;
+            case R.id.register_self:
+                break;
+        }
+    }
+}
